e87c40ba52d97bee754b6ea05206d983dcd97a17
Martin Petera <martin.petera@gmail.com>
2019-03-09 13:22:18 +0100

    Added HID_keycodes definition and KeyAction factory method to Java project

---
+++ android/HumanPad/app/src/main/java/cz/butab/humanpad/HIDKeyCodes.java
@@ -0,0 +1,15 @@
+package cz.butab.humanpad;
+
+public class HIDKeyCodes {
+    public static final int Char_W = 0x1a;     // DEC: 26
+    public static final int Char_A = 0x04;     // DEC: 4
+    public static final int Char_S = 0x16;     // DEC: 22
+    public static final int Char_D = 0x07;     // DEC: 7
+
+    public static final int Char_E = 0x08;     // DEC: 8
+
+    public static final int Arrow_Up = 0x52;     // DEC: 82
+    public static final int Arrow_Down = 0x51;     // DEC: 81
+    public static final int Arrow_Left = 0x50;     // DEC: 80
+    public static final int Arrow_Right = 0x4f;     // DEC: 79
+}
